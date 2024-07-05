c36f12e663574b57c3804822809d0363cf419494
erikamagnaye <147479942+eka31@users.noreply.github.com>
2024-07-05 17:46:07 +0800

    ship added

--- lib/screens/Home/Widget/home_app_bar.dart
+++ lib/screens/Home/Widget/home_app_bar.dart
@@ -20,6 +20,8 @@ class CustomAppBar extends StatelessWidget {
           iconSize: 28,
           icon: const Icon(Icons.grid_view_outlined),
         ),
+        Row(
+          children: [
         Stack(
           children: [
             IconButton(
@@ -55,7 +57,136 @@ class CustomAppBar extends StatelessWidget {
             ),
           ],
         ),
+           IconButton(
+            style: IconButton.styleFrom(
+              backgroundColor: kcontentColor,
+              padding: const EdgeInsets.all(15),
+            ),
+            onPressed: ( ) {
+              Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => OrderDetailsScreen()),
+                  );
+            },
+            iconSize: 30,
+            icon: const Icon(Icons.local_shipping_outlined),  //icon na truck sa homepage
+          ),
       ],
+        ),
+      ],
+    );
+ 
+    
+  }
+}
+
+
+class OrderDetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Order Details'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Image.asset(
+                    "images/all/sweet.png",
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Order Status: For Pickup",
+                        style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Pickup Date: 2023-03-15",
+                        style: TextStyle(fontSize: 9),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10), // Add some space between the images
+                Expanded(
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        "images/all/wireless.png",
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "seller is packing your order",
+                        style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Pickup Date: 2023-03-15",
+                        style: TextStyle(fontSize: 9),
+                      ),
+                    ],
+                  ),
+                ),
+                     
+              ],
+            ),
+                        Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Image.asset(
+                    "images/all/miband.jpg",
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Order Status: Cancelled",
+                        style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Pickup Date: 2023-03-15",
+                        style: TextStyle(fontSize: 9),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10), // Add some space between the images
+                Expanded(
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        "images/all/jacket.png",
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Order Status: Claimed",
+                        style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Claimed Date: 2023-03-15",
+                        style: TextStyle(fontSize: 9),
+                      ),
+                    ],
+                  ),
+                ),
+                     
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
